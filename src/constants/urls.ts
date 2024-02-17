03def800b0ae4a186123a505268ede721c2c5d1e
OceanOwl <115832268+OceanOwl@users.noreply.github.com>
2024-02-17 15:17:27 +0100

    hw_1

---
+++ src/constants/urls.ts
@@ -0,0 +1,14 @@
+const baseURL:string = 'https://jsonplaceholder.typicode.com/'
+
+const users = '/users'
+const urls ={
+   users:{
+       base:users,
+       byId:(id:number):string=>`${users}/${id}`
+   }
+}
+
+export {
+    baseURL,
+    urls
+}
